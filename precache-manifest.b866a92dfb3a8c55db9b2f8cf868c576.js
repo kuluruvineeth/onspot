3aeecd13d21d5ab5298522f4de8ce0c1350bc8c2
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-03-19 12:23:50 +0530

    Updates

---
+++ precache-manifest.b866a92dfb3a8c55db9b2f8cf868c576.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81f9b73692106850ff6a10ac244fd775",
+    "url": "/onspot/index.html"
+  },
+  {
+    "revision": "3d70ea57a52749f2665d",
+    "url": "/onspot/static/css/2.142df03a.chunk.css"
+  },
+  {
+    "revision": "1261198bdabb10454e79",
+    "url": "/onspot/static/css/main.07331a38.chunk.css"
+  },
+  {
+    "revision": "3d70ea57a52749f2665d",
+    "url": "/onspot/static/js/2.d728d91d.chunk.js"
+  },
+  {
+    "revision": "1ce05011c5e5ab464837999d7fb28887",
+    "url": "/onspot/static/js/2.d728d91d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1261198bdabb10454e79",
+    "url": "/onspot/static/js/main.f73173f0.chunk.js"
+  },
+  {
+    "revision": "bb3599a69b095f7364dd",
+    "url": "/onspot/static/js/runtime-main.507dfc42.js"
+  }
+]);
